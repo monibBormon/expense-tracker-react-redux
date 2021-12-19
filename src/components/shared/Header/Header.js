1bfbf7e7c62b65d1659a831a7103f92e6ad0fe7c
monibBormon <monibbormon14@gmail.com>
2021-12-19 20:38:20 +0600

    link added

--- src/components/shared/Header/Header.js
+++ src/components/shared/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css'
 
 const Header = () => {
@@ -6,7 +7,9 @@ const Header = () => {
         <div className='header-container w-full px-5 lg:px-0 lg:w-1/2 mx-auto py-3 items-center'>
             <div className="header flex justify-between items-center">
                 <div className="logo text-2xl lg:text-3xl font-semibold text-white">
-                    Expense <span className='text-black'>Tracker.</span>
+                    <Link to='/'>
+                        Expense <span className='text-black'>Tracker.</span>
+                    </Link>
                 </div>
                 <div className="nav">
                     <h2 className='text-white font-semibold text-lg'>Login/Register</h2>
